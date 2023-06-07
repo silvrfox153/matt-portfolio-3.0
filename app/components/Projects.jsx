2d651b886b58e407cf5e1780f754ed634e027192
silvrfox153 <matt.tierney153@gmail.com>
2023-06-07 13:50:24 -0400

    Moved logos to top on mobile

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -18,6 +18,11 @@ const Projects = () => {
           {/* ===== PADDYS LUNCH ===== */}
           <li className="mt-12 mb-12">
             <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+              <Image
+                src={paddysLogo}
+                alt="Paddy's Lunch Logo"
+                className="z-10 w-72 mx-auto sm:w-[full]"
+              />
               <div className="z-10 sm:order-2 sm:col-span-6">
                 <h3>
                   <Link
@@ -55,16 +60,16 @@ const Projects = () => {
 
                 <p className="mt-2 text-sm">$50,000 avg. annual sales</p>
               </div>
-              <Image
-                src={paddysLogo}
-                alt="Paddy's Lunch Logo"
-                className="z-10 w-72 mx-auto sm:w-[full]"
-              />
             </div>
           </li>
           {/* ===== LEDUC ARTS & ANTIQUES ===== */}
           <li className="mb-12">
             <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+              <Image
+                src={leducLogo}
+                alt="Leduc Art & Antiques Logo"
+                className="z-10 w-72 mx-auto sm:w-[full]"
+              />
               <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
               <div className="z-10 sm:order-2 sm:col-span-6">
                 <h3>
@@ -102,16 +107,16 @@ const Projects = () => {
                   store is currently under development.
                 </p>
               </div>
-              <Image
-                src={leducLogo}
-                alt="Leduc Art & Antiques Logo"
-                className="z-10 w-72 mx-auto sm:w-[full]"
-              />
             </div>
           </li>
           {/* ===== JOE MCGUIRK ===== */}
           <li className="mb-12">
             <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+              <Image
+                src={mcGuirkLogo}
+                alt="Joe McGuirk Campaign Logo"
+                className="z-10 w-72 mx-auto sm:w-[full]"
+              />
               <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
               <div className="z-10 sm:order-2 sm:col-span-6">
                 <h3>
@@ -150,16 +155,16 @@ const Projects = () => {
                   Analytics.
                 </p>
               </div>
-              <Image
-                src={mcGuirkLogo}
-                alt="Joe McGuirk Campaign Logo"
-                className="z-10 w-72 mx-auto sm:w-[full]"
-              />
             </div>
           </li>
           {/* ===== TIMMY SULLIVAN ===== */}
           <li className="mb-12">
             <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+              <Image
+                src={timmyLogo}
+                alt="Timmy Sullivan Logo"
+                className="z-10 w-72 mx-auto sm:w-[full]"
+              />
               <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
               <div className="z-10 sm:order-2 sm:col-span-6">
                 <h3 className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-sky-300 focus-visible:text-sky-300 undefined group/link text-base">
@@ -191,16 +196,16 @@ const Projects = () => {
                   Site retired at end of campaign in 2022
                 </p>
               </div>
-              <Image
-                src={timmyLogo}
-                alt="Timmy Sullivan Logo"
-                className="z-10 w-72 mx-auto sm:w-[full]"
-              />
             </div>
           </li>
           <li className="mb-12">
             {/* ===== RUTH RYAN ALLEN ===== */}
             <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+              <Image
+                src={ruthRyanLogo}
+                alt="Ruth Ryan Allen Logo"
+                className="z-10 w-72 mx-auto sm:w-[full]"
+              />
               <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
               <div className="z-10 sm:order-2 sm:col-span-6">
                 <h3 className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-sky-300 focus-visible:text-sky-300 undefined group/link text-base">
@@ -231,15 +236,16 @@ const Projects = () => {
                 </p>
                 <p className="text-sm mt-2">Site retired in 2022.</p>
               </div>
-              <Image
-                src={ruthRyanLogo}
-                alt="Ruth Ryan Allen Logo"
-                className="z-10 w-72 mx-auto sm:w-[full]"
-              />
             </div>
           </li>
           <li className="mb-12">
+            {/* ===== SEAN TIERNEY ===== */}
             <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+              <Image
+                src={seanTierneyLogo}
+                alt="Sean Tierney Logo"
+                className="z-10 w-72 mx-auto sm:w-[full]"
+              />
               <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
               <div className="z-10 sm:order-2 sm:col-span-6">
                 <h3 className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-sky-300 focus-visible:text-sky-300 undefined group/link text-base">
@@ -270,11 +276,6 @@ const Projects = () => {
                 </p>
                 <p className="text-sm mt-2">Site retired at in 2019.</p>
               </div>
-              <Image
-                src={seanTierneyLogo}
-                alt="Sean Tierney Logo"
-                className="z-10 w-72 mx-auto sm:w-[full]"
-              />
             </div>
           </li>
         </ul>
