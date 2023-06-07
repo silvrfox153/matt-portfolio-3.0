a37b4cda3d0de5268e4e5a55f7586fa0cd05ffd3
silvrfox153 <matt.tierney153@gmail.com>
2023-06-07 16:51:50 -0400

    Added light background to projects on mobile view

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -16,20 +16,23 @@ const Projects = () => {
       <div className="px-5">
         <ul className="group/list">
           {/* ===== PADDYS LUNCH ===== */}
-          <li className="mt-12 mb-12">
-            <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+          <li className="mb-12">
+            <div className="group relative grid gap-4 p-6 sm:pb-1 rounded-lg transition-all bg-slate-800 sm:bg-transparent sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
               <Image
                 src={paddysLogo}
                 alt="Paddy's Lunch Logo"
                 className="z-10 w-72 mx-auto sm:w-[full]"
               />
+
+              <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+
               <div className="z-10 sm:order-2 sm:col-span-6">
                 <h3>
                   <Link
-                    href="https://paddys34.com/"
                     target="_blank"
                     rel="#"
-                    aria-label="Paddy's Lunch"
+                    aria-label="Paddy's Lucnh"
+                    href="https://paddys34.com/"
                     className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-sky-300 focus-visible:text-sky-300 group/link text-base"
                   >
                     <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
@@ -54,25 +57,24 @@ const Projects = () => {
                   </Link>
                 </h3>
                 <p className="text-slate-400 mt-2 text-sm leading-normal">
-                  An ecommerce developed website using WordPress, Elementor, and
-                  Shipstation. Technologies included: Google Analytics, HubSpot.
-                </p>
-
-                <p className="mt-2 text-sm text-slate-400">
-                  $50,000 avg. annual sales
+                  Built and continue to maintain website using WordPress, and
+                  Elementor. Designed business logo. Google Analytics. Ecommerce
+                  store is currently under development.
                 </p>
               </div>
             </div>
           </li>
           {/* ===== LEDUC ARTS & ANTIQUES ===== */}
           <li className="mb-12">
-            <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+            <div className="group relative grid gap-4 p-6 sm:pb-1 rounded-lg transition-all bg-slate-800 sm:bg-transparent sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
               <Image
                 src={leducLogo}
                 alt="Leduc Art & Antiques Logo"
                 className="z-10 w-72 mx-auto sm:w-[full]"
               />
+
               <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+
               <div className="z-10 sm:order-2 sm:col-span-6">
                 <h3>
                   <Link
@@ -113,7 +115,7 @@ const Projects = () => {
           </li>
           {/* ===== JOE MCGUIRK ===== */}
           <li className="mb-12">
-            <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+            <div className="group relative grid gap-4 p-6 sm:pb-1 rounded-lg transition-all bg-slate-800 sm:bg-transparent sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
               <Image
                 src={mcGuirkLogo}
                 alt="Joe McGuirk Campaign Logo"
@@ -161,7 +163,7 @@ const Projects = () => {
           </li>
           {/* ===== TIMMY SULLIVAN ===== */}
           <li className="mb-12">
-            <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+            <div className="group relative grid gap-4 p-6 sm:pb-1 rounded-lg transition-all bg-slate-800 sm:bg-transparent sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
               <Image
                 src={timmyLogo}
                 alt="Timmy Sullivan Logo"
@@ -202,7 +204,7 @@ const Projects = () => {
           </li>
           <li className="mb-12">
             {/* ===== RUTH RYAN ALLEN ===== */}
-            <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+            <div className="group relative grid gap-4 p-6 sm:pb-1 rounded-lg transition-all bg-slate-800 sm:bg-transparent sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
               <Image
                 src={ruthRyanLogo}
                 alt="Ruth Ryan Allen Logo"
@@ -244,7 +246,7 @@ const Projects = () => {
           </li>
           <li className="mb-12">
             {/* ===== SEAN TIERNEY ===== */}
-            <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+            <div className="group relative grid gap-4 p-6 sm:pb-1 rounded-lg transition-all bg-slate-800 sm:bg-transparent sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
               <Image
                 src={seanTierneyLogo}
                 alt="Sean Tierney Logo"
