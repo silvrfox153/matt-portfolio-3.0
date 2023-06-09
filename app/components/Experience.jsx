8b73366e2ed74ab4d0f9c03da428a39550c61c6c
silvrfox153 <matt.tierney153@gmail.com>
2023-06-09 12:08:58 -0400

    Ajusted padding to experience heading

--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -3,14 +3,14 @@ import Link from 'next/link';
 const Experience = () => {
   return (
     <section id="experience" className="pt-5">
-      <div className="py-2 px-5">
+      <div className="px-5">
         <h2 className="uppercase text-xl text-white font-semibold">
           Work Experience
         </h2>
       </div>
       <div className="px-5">
         <ol className="group/list">
-          <li className="my-12">
+          <li className="my-6">
             <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
               <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
               <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-1">
