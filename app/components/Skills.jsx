c0be834c90fccd4aa1c584ca6e1cb30d7343cf19
silvrfox153 <matt.tierney153@gmail.com>
2023-06-09 12:26:34 -0400

    added linkedin icon

---
+++ app/components/Skills.jsx
@@ -0,0 +1,11 @@
+const Skills = () => {
+  return (
+    <section id="experience" className="pt-5">
+      <div className="px-5">
+        <h2 className="uppercase text-xl text-white font-semibold">Skills</h2>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
