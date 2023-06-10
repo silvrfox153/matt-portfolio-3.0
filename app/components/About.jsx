b1644ecaa90cd2c28cd91263fe70c8dade9f76d3
silvrfox153 <matt.tierney153@gmail.com>
2023-06-09 20:14:26 -0400

    removed unnecessary div

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,23 +1,21 @@
 const About = () => {
   return (
     <section id="about" className="p-5">
-      <div>
-        <div className="text-slate-400">
-          <p className="my-4 tracking-tight leading-snug">
-            In the summer of 2016, my employer announced a plan to restructure
-            the operations organization. Part of that plan was to transition my
-            team’s responsibilities to the Ireland office by the end of the
-            year, thereby eliminating the Real Time Operations team in Boston.
-            In February of 2017, my brother Sean talked to me about his interest
-            in running for Cambridge City Council, he needed a campaign manager
-            and a website. At that time, I knew nothing about running a
-            political campaign or how to build a website. But because I needed a
-            new project and because it was my brother, I dove right in. Fast
-            forward to today and I&apos;ve had the privilege of managing three
-            political campaigns, building four campaign websites, an e-commerce
-            website, and a small business website.
-          </p>
-        </div>
+      <div className="text-slate-400">
+        <p className="my-4 tracking-tight">
+          In the summer of 2016, my employer announced a plan to restructure the
+          operations organization. Part of that plan was to transition my team’s
+          responsibilities to the Ireland office by the end of the year, thereby
+          eliminating the Real Time Operations team in Boston. In February of
+          2017, my brother Sean talked to me about his interest in running for
+          Cambridge City Council, he needed a campaign manager and a website. At
+          that time, I knew nothing about running a political campaign or how to
+          build a website. But because I needed a new project and because it was
+          my brother, I dove right in. Fast forward to today and I&apos;ve had
+          the privilege of managing three political campaigns, building four
+          campaign websites, an e-commerce website, and a small business
+          website.
+        </p>
       </div>
     </section>
   );
