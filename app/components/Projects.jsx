29d84038cc0cd1d1fcaf5bb573936d58762b10db
silvrfox153 <matt.tierney153@gmail.com>
2023-06-09 21:24:58 -0400

    Added silvrfox portfolio to projects component

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -6,6 +6,7 @@ import mcGuirkLogo from '/public/assets/logos/joe-mcguirk-logo.svg';
 import timmyLogo from '/public/assets/logos/timmy-sullivan-logo.svg';
 import ruthRyanLogo from '/public/assets/logos/ruth-ryan-allen-logo.svg';
 import seanTierneyLogo from '/public/assets/logos/sean-tierney-logo.svg';
+import silvrfoxLogo from '/public/assets/logos/silvrfox-logo.svg';
 
 const Projects = () => {
   return (
@@ -283,6 +284,43 @@ const Projects = () => {
                 </p>
                 <p className="text-sm mt-2 text-slate-400">
                   Site retired at in 2019.
+                </p>
+              </div>
+            </div>
+          </li>
+          <li className="mb-12">
+            {/* ===== MATT PORTFOLIO ===== */}
+            <div className="group relative grid gap-4 p-6 sm:pb-1 rounded-lg transition-all bg-slate-800 sm:bg-transparent sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+              <Image
+                src={silvrfoxLogo}
+                alt="Silvr Fox Logo"
+                className="z-10 w-72 mx-auto sm:w-[full]"
+              />
+              <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+              <div className="z-10 sm:order-2 sm:col-span-6">
+                <h3 className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-sky-300 focus-visible:text-sky-300 undefined group/link text-base">
+                  <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                  <span>
+                    Matt Tierney Portfolio
+                    <span className="inline-block">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                          clipRule="evenodd"
+                        ></path>
+                      </svg>
+                    </span>
+                  </span>
+                </h3>
+                <p className="text-slate-400 mt-2 text-sm leading-normal">
+                  Built with Next.js and Tailwind CSS. Deployed with Vercel.
                 </p>
               </div>
             </div>
