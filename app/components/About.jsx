7879edcf63302a15e1f47c7980ab3d9b83241040
silvrfox153 <matt.tierney153@gmail.com>
2023-12-05 10:11:38 -0500

    Updated Summary About section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,20 +4,6 @@ const About = () => {
       <div>
         <div className="text-slate-400">
           <p className="my-4 tracking-tight leading-snug">
-            In the summer of 2016, my employer announced a plan to restructure
-            the operations organization. Part of that plan was to transition my
-            team’s responsibilities to the Ireland office by the end of the
-            year, thereby eliminating the Real Time Operations team in Boston.
-            In February of 2017, my brother Sean talked to me about his interest
-            in running for Cambridge City Council, he needed a campaign manager
-            and a website. At that time, I knew nothing about running a
-            political campaign or how to build a website. But because I needed a
-            new project and because it was my brother, I dove right in. Fast
-            forward to today and I&apos;ve had the privilege of managing three
-            political campaigns, building four campaign websites, an e-commerce
-            website, and a small business website.
-          </p>
-          <p>
             I am an operations leader with a passion for finding and solving
             challenging problems with great people. I have experience in driving
             innovative solutions through process improvement, relationship
