198c844f4c975cd044993ebefe9882f6b605c578
silvrfox153 <matt.tierney153@gmail.com>
2023-12-19 13:15:52 -0500

    Changes Inkscape to Affinity Design

--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -61,7 +61,7 @@ const Experience = () => {
                   </li>
                   <li className="mr-1.5 mt-2">
                     <div className="flex items-center rounded-full bg-sky-400/10 px-3 py-1 text-xs font-medium leading-5 text-sky-300 ">
-                      Inkscape
+                      Affinity Design
                     </div>
                   </li>
                   <li className="mr-1.5 mt-2">
